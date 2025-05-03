ee7d0324a89b01ba9cdd26cb9926c2527966eb4b
Биткин А.В. <bitkin125@gmail.com>
2025-05-03 13:38:31 +0300

    Commit 1

--- Airplane.h
+++ Airplane.h
@@ -1,1 +1,19 @@
 #pragma once
+#include<string>
+
+class Airplane {
+private:
+	std::string type;
+	int vpp_lenght;
+	int time;
+	int max_circles;
+
+	std::string request;
+	int circle = 0;
+
+public:
+	Airplane(std::string type_, std::string request_);
+
+	void set_data(int vpp_lenght_, int time_, int max_circles_);
+
+};
