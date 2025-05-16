279e6bb3d2fc9be929b96fb9de6ca1cfec817269
abchihba0 <113386115+abchihba0@users.noreply.github.com>
2025-05-17 01:55:15 +0300

    new file:   build/.cmake/api/v1/query/client-vscode/query.json 	new file:   build/.cmake/api/v1/reply/cache-v2-9bc5fc38124a2b6a78f7.json 	new file:   build/.cmake/api/v1/reply/cmakeFiles-v1-291452db2c035e570d19.json 	new file:   build/.cmake/api/v1/reply/codemodel-v2-3dad7e1220caeff87ef2.json 	new file:   build/.cmake/api/v1/reply/directory-.-d0094a50bb2071803777.json 	new file:   build/.cmake/api/v1/reply/directory-imguiLibrary.imgui-b827a71acc0b6facab83.json 	new file:   build/.cmake/api/v1/reply/index-2025-05-16T22-55-04-0725.json 	new file:   build/.cmake/api/v1/reply/target-imgui-96ce35fe56e870082de3.json 	new file:   build/.cmake/api/v1/reply/target-my_app-ad72ae73d888c7ec7b82.json 	new file:   build/.cmake/api/v1/reply/toolchains-v1-d83f33300efd1b808b56.json 	modified:   build/CMakeCache.txt 	renamed:    build/CMakeFiles/3.31.0/CMakeCCompiler.cmake -> build/CMakeFiles/3.30.4/CMakeCCompiler.cmake 	renamed:    build/CMakeFiles/3.31.0/CMakeCXXCompiler.cmake -> build/CMakeFiles/3.30.4/CMakeCXXCompiler.cmake 	renamed:    build/CMakeFiles/3.31.0/CMakeDetermineCompilerABI_C.bin -> build/CMakeFiles/3.30.4/CMakeDetermineCompilerABI_C.bin 	renamed:    build/CMakeFiles/3.31.0/CMakeDetermineCompilerABI_CXX.bin -> build/CMakeFiles/3.30.4/CMakeDetermineCompilerABI_CXX.bin 	renamed:    build/CMakeFiles/3.31.0/CMakeRCCompiler.cmake -> build/CMakeFiles/3.30.4/CMakeRCCompiler.cmake 	renamed:    build/CMakeFiles/3.31.0/CMakeSystem.cmake -> build/CMakeFiles/3.30.4/CMakeSystem.cmake 	renamed:    build/CMakeFiles/3.31.0/CompilerIdC/CMakeCCompilerId.c -> build/CMakeFiles/3.30.4/CompilerIdC/CMakeCCompilerId.c 	renamed:    build/CMakeFiles/3.31.0/CompilerIdC/a.exe -> build/CMakeFiles/3.30.4/CompilerIdC/a.exe 	renamed:    build/CMakeFiles/3.31.0/CompilerIdCXX/CMakeCXXCompilerId.cpp -> build/CMakeFiles/3.30.4/CompilerIdCXX/CMakeCXXCompilerId.cpp 	renamed:    build/CMakeFiles/3.31.0/CompilerIdCXX/a.exe -> build/CMakeFiles/3.30.4/CompilerIdCXX/a.exe 	modified:   build/CMakeFiles/CMakeConfigureLog.yaml 	modified:   build/CMakeFiles/CMakeDirectoryInformation.cmake 	modified:   build/CMakeFiles/Makefile.cmake 	modified:   build/CMakeFiles/Makefile2 	modified:   build/CMakeFiles/my_app.dir/build.make 	deleted:    build/CMakeFiles/my_app.dir/compiler_depend.internal 	modified:   build/CMakeFiles/my_app.dir/compiler_depend.make 	modified:   build/CMakeFiles/my_app.dir/flags.make 	deleted:    build/CMakeFiles/my_app.dir/imguiLibrary/imgui/backends/imgui_impl_glfw.cpp.obj 	deleted:    build/CMakeFiles/my_app.dir/imguiLibrary/imgui/backends/imgui_impl_glfw.cpp.obj.d 	deleted:    build/CMakeFiles/my_app.dir/imguiLibrary/imgui/backends/imgui_impl_opengl3.cpp.obj 	deleted:    build/CMakeFiles/my_app.dir/imguiLibrary/imgui/backends/imgui_impl_opengl3.cpp.obj.d 	modified:   build/CMakeFiles/my_app.dir/includes_CXX.rsp 	modified:   build/CMakeFiles/my_app.dir/link.txt 	modified:   build/CMakeFiles/my_app.dir/linkLibs.rsp 	deleted:    build/CMakeFiles/my_app.dir/main.cpp.obj 	deleted:    build/CMakeFiles/my_app.dir/main.cpp.obj.d 	deleted:    build/CMakeFiles/my_app.dir/objects.a 	modified:   build/Makefile 	modified:   build/cmake_install.cmake 	modified:   build/imguiLibrary/imgui/CMakeFiles/CMakeDirectoryInformation.cmake 	modified:   build/imguiLibrary/imgui/CMakeFiles/imgui.dir/build.make 	deleted:    build/imguiLibrary/imgui/CMakeFiles/imgui.dir/compiler_depend.internal 	modified:   build/imguiLibrary/imgui/CMakeFiles/imgui.dir/compiler_depend.make 	modified:   build/imguiLibrary/imgui/CMakeFiles/imgui.dir/flags.make 	modified:   build/imguiLibrary/imgui/CMakeFiles/imgui.dir/includes_CXX.rsp 	modified:   build/imguiLibrary/imgui/CMakeFiles/imgui.dir/link.txt 	modified:   build/imguiLibrary/imgui/Makefile 	modified:   build/imguiLibrary/imgui/cmake_install.cmake
    Changes not staged for commit:
    	modified:   imguiLibrary/imgui (untracked content)

--- build/cmake_install.cmake
+++ build/cmake_install.cmake
@@ -34,7 +34,7 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/msys64/ucrt64/bin/objdump.exe")
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
@@ -42,12 +42,6 @@ if(NOT CMAKE_INSTALL_LOCAL_ONLY)
   include("D:/hse/c++/airport/fizhmaAirport/build/imguiLibrary/imgui/cmake_install.cmake")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/hse/c++/airport/fizhmaAirport/build/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -61,6 +55,8 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
+  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
   file(WRITE "D:/hse/c++/airport/fizhmaAirport/build/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
--- build/imguiLibrary/imgui/cmake_install.cmake
+++ build/imguiLibrary/imgui/cmake_install.cmake
@@ -34,12 +34,6 @@ endif()
 
 # Set path to fallback-tool for dependency-resolution.
 if(NOT DEFINED CMAKE_OBJDUMP)
-  set(CMAKE_OBJDUMP "C:/msys64/mingw64/bin/objdump.exe")
+  set(CMAKE_OBJDUMP "C:/msys64/ucrt64/bin/objdump.exe")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/hse/c++/airport/fizhmaAirport/build/imguiLibrary/imgui/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
