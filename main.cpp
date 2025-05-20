20d4c9047e6c1c4a29e0e5c8280368e1f3699069
Daria-Kurshakova <dariakurshakova148@gmail.com>
2025-05-20 19:06:43 +0300

    .

--- main.cpp
+++ main.cpp
@@ -9,6 +9,7 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "GLFW/glfw3.h"
+//ну мне очевидно нужны какие-то коммиты
 
 int main() {
     // Инициализация базы данных и менеджера авторизации
