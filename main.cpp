8d2657d503c5ae411ffc7ff531097822fc0c6dc7
Daria-Kurshakova <dariakurshakova148@gmail.com>
2025-05-20 19:10:56 +0300

    Подключение графики

--- main.cpp
+++ main.cpp
@@ -9,8 +9,7 @@
 #include "backends/imgui_impl_glfw.h"
 #include "backends/imgui_impl_opengl3.h"
 #include "GLFW/glfw3.h"
-// ну ладно ещё парочку коммитов
-// ещё совсем чуть-чуть
+
 int main() {
     // Инициализация базы данных и менеджера авторизации
     Database db("game_progress.db");
