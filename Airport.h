cd7a4a66f83ef08503ce84d844ff06508cb8b433
Биткин А.В. <bitkin125@gmail.com>
2025-05-22 21:53:55 +0300

    Commit 1

--- Airport.h
+++ Airport.h
@@ -2,27 +2,40 @@
 #include <vector>
 #include "Airplane.h"
 #include "VPP.h"
+#include <queue>
+// количество уровней нужно продумать, оно должно быть нам известно, в примере ниже уровней допустим будет 5
+
+// компаратор для очереди
+struct CompareByTimeOnTheCircle {
+	bool operator()(Airplane* a, Airplane* b) {
+		return a->getTime() > b->getTime();
+	}
+};
+
+// создаем структуру, которая будет хранить прогресс на уровне, то есть массив с занятостью vpps в данный момент, ОЧЕРЕДЬ manager из самолетов на следующих кругах 
+struct LevelProgress {
+	int Level = 1;
+	int vpp_count;
+	std::vector<VPP*>vpps; // массив из впп
+	// std::vector<Airplane*> manager;// в этот массив будут добавляться запросы 
+	std::priority_queue<Airplane*,std::vector<Airplane*>,CompareByTimeOnTheCircle> managerLanding;// в этот массив будут добавляться запросы на посадку
+	std::priority_queue<Airplane*,std::vector<Airplane*>,CompareByTimeOnTheCircle> managerRise;// в этот массив будут добавляться запросы на взлёт
+	int countOfProcessedRequests = 0; // количество обработанных запросов
+	int countOfCorrectProcessedRequests = 0; // количество тех запросов, которые были обработанны корректно(то есть самолет был посажен или взлетел(те запросы, которые хранят самолеты на следующих кругах ни там ни там, они в доп массиве manager))
+};
+
 
 class Airport {
-private:
-	int vpp_count;
-	int plane_count;
-	std::vector<VPP*>vpps;
-	std::vector<Airplane*>manager;
+	private:std::vector<int>countOfReQuestsOnTheLevel = {15, 20, 25, 30, 35}; // количество запросов на разных уровнях
+		const std::vector<int> runwaysPerLevel = {3, 5, 7, 7, 7}; // Количество полос для каждого уровня
+		std::vector<LevelProgress*>MemoryAboutLevelsProgress; // надо крч такой вектор чтобы можно было оттуда доставать прогресс уровня из структуры сразу, то есть по умолчанию все значения ноль и потом при выходе из игры прогрес перезаписывается
+	public:
+		Airport();
+		Airplane* set_manager(LevelProgress* ourLevel);
+		int processing(Airplane* tempPlane, LevelProgress* ourLevel);
+		void game(LevelProgress* ourLevel);
+		void gameProcessing(std::string point,  int tempLvl);
 
-public:
 
-	Airport();
-
-	void set_vpp_count(int count_) { vpp_count = count_; }
-	void set_plane_count(int count_) { plane_count = count_; }
-	void set_vpps(std::vector<int>lenghts_);
-
-	int get_vpp_count()const { return vpp_count; }
-	int get_plane_count()const { return plane_count; }
-	void get_vpps()const;
-
-	void set_manager(std::vector<int>types_);
-	Airplane* set_manager();
-	void get_manager()const;
+		std::vector<LevelProgress*> returnMemory() {return MemoryAboutLevelsProgress;}
 };
--- VPP.h
+++ VPP.h
@@ -4,27 +4,14 @@ class VPP {
     private:
         int lenght;
         bool status;
+        unsigned int busytime = 0;
     public:
         VPP(int lenght_);
-    
+
         void set_lenght(int lenght_) { lenght = lenght_; }
         void set_status(int status_) { status = status_; }
-    
+        int getBusyTime()const {return busytime;}
+        void setBusyTime(int time) {busytime = time;}
         int get_lenght()const { return lenght; }
         bool get_status()const { return status; }
     };
-#pragma once
-
-class VPP {
-private:
-    int lenght;
-    bool status;
-public:
-    VPP(int lenght_);
-
-    void set_lenght(int lenght_) { lenght = lenght_; }
-    void set_status(int status_) { status = status_; }
-
-    int get_lenght()const { return lenght; }
-    bool get_status()const { return status; }
-};
